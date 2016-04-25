2cb530eea377523813068c5ac7a3faf82903b195
VladimirGM <v.m.gryaznov@gmail.com>
2016-04-25 15:49:44 +0300

    Update ScaleFree.py
    работать должна

--- ScaleFree.py
+++ ScaleFree.py
@@ -5,7 +5,6 @@ def new_scale_graph(num, gammat):
     G = nx.scale_free_graph(num, 0.4, 0.59, 0.01, gammat, 0, None, None)
     nx.draw(G)
     plt.savefig("1.png")
-    plt.show()
     return G
 
 
